af8f1048918e90e47b36f587c3f70d52ba684161
Spikree <avimahalingpure10@gmail.com>
2024-03-10 21:02:52 +0530

    complected sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
+const selectedCategory = "New";
 
 function Sidebar() {
   return (
@@ -12,9 +13,29 @@ function Sidebar() {
         }}
       >
         {categories.map((category) => (
-          <button key={category.name} className="category-btn">
-            <span>{category.icon}</span>
-            <span>{category.name}</span>
+          <button
+            key={category.name}
+            className="category-btn"
+            style={{
+              background: category.name === selectedCategory && "#FC1503",
+              color: "white",
+            }}
+          >
+            <span
+              style={{
+                color: category.name === selectedCategory ? "white" : "red",
+                marginRight: "15px",
+              }}
+            >
+              {category.icon}
+            </span>
+            <span
+              style={{
+                opacity: category.name === selectedCategory ? "1" : "0.8",
+              }}
+            >
+              {category.name}
+            </span>
           </button>
         ))}
       </Stack>
